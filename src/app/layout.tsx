4ce355b7bcb2e7c0e6272abda4a8fbc1b52ae23a
Guilherme Knorst <guia.knorst@gmail.com>
2025-08-24 18:17:41 -0300

    Add vercel speedinsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 import Header from "@/components/Header";
 
@@ -19,6 +20,7 @@ export default function RootLayout({
 				<Header />
         {children}
 				<Analytics />
+				<SpeedInsights/>
       </body>
     </html>
   );
