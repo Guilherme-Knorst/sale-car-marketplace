136e914b7055f835a0de9858d1b3a0b76ff1000b
Guilherme Knorst <guia.knorst@gmail.com>
2025-08-31 17:02:39 -0300

    Add caching and page metadata

--- src/app/cars/[id]/page.tsx
+++ src/app/cars/[id]/page.tsx
@@ -4,6 +4,52 @@ import { connectToDatabase } from '@/lib/mongodb';
 import Car, { ICar } from '@/models/Car';
 import Link from 'next/link';
 import { notFound } from 'next/navigation';
+import { Metadata } from "next";
+
+export async function generateStaticParams() {
+  await connectToDatabase();
+  const cars = await Car.find({ active: true }, "_id");
+  return cars.map((car) => ({ id: car._id.toString() }));
+}
+
+export const revalidate = 300; // revalida a cada 5 minutos segundos
+
+
+interface Props {
+  params: { id: string };
+}
+
+// 🔹 Gera meta tags dinâmicas para cada carro
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  await connectToDatabase();
+  const car: ICar | null = await Car.findById<ICar>(params.id);
+
+  if (!car) {
+    return {
+      title: "Carro não encontrado",
+      description: "Esse carro não está mais disponível.",
+    };
+  }
+
+  return {
+    title: `${car.brand} ${car.carModel} ${car.year} à venda - Sale Projetos`,
+    description: `Motor ${car.engine} - ${car.fuel} - ${car.mileage.toLocaleString("pt-BR")} km`,
+    openGraph: {
+      title: `${car.brand} ${car.carModel} ${car.year} à venda - Sale Projetos`,
+      description: car.description[0].text,
+      url: `https://saleprojetos.com/cars/${car._id}`,
+      images: [
+        {
+          url: car.mainImage, // garante URL absoluta
+          width: 1200,
+          height: 630,
+          alt: `${car.brand} ${car.carModel}`,
+        },
+      ],
+    },
+  };
+}
+
 
 export default async function CarDetailPage({params}: {params: Promise<{ id: string }>}) {
   await connectToDatabase();
